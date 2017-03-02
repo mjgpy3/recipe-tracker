5dbcbecdf5031430560e037048db7576c80deff6
Michael Gilliland <mjg.py3@gmail.com>
2017-03-02 07:27:06 -0500

    Move to welcome screen.

--- ui/Main.elm
+++ ui/Main.elm
@@ -3,25 +3,54 @@ import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 main =
-  Html.beginnerProgram { model = 0, view = view, update = update }
+  Html.beginnerProgram { model = NoneYet, view = view, update = update }
 
-type Msg = Increment | Decrement
+type alias ShortName = String
+type Role = Chef | Diner
+
+type alias User = (ShortName, Role)
+
+type Model
+  = NoneYet
+  | Welcome User
+
+type Msg = SelectUser User
 
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    SelectUser user -> Welcome user
 
-    Decrement ->
-      model - 1
+users =
+  [ ("Thelma", Chef)
+  , ("Jimmy", Diner)
+  , ("Guest", Diner)
+  ]
 
 view model =
+  case model of
+    NoneYet -> viewSelectUser model
+
+    Welcome user -> viewWelcome user
+
+selectUserButton (name, role) = button [class "btn btn-primary btn-block", onClick (SelectUser (name, role))] [text name]
+
+viewSelectUser model =
   div []
     [ header [class "bar bar-nav"]
-        [h1 [class "title"] [text "Who Are You?"]]
+      [h1 [class "title"] [text "Who Are You?"]]
     , div [class "content content-padded"]
-        [ button [class "btn btn-primary btn-block"] [text "Her"]
-        , button [class "btn btn-primary btn-block"] [text "Him"]
-        , button [class "btn btn-primary btn-block"] [text "Guest"]
-        ]
+      (List.map selectUserButton users)
+    ]
+
+roleText role =
+  case role of
+    Diner -> "diner"
+    Chef -> "chef"
+
+viewWelcome (name, role) =
+  div []
+    [ header [class "bar bar-nav"]
+      [h1 [class "title"] [text ("Welcome " ++ roleText role ++ " " ++ name)]]
+    , div [class "content content-padded"]
+      []
     ]
