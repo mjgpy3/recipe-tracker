e0035e8047671df8a55b848b6392f6ad2c9a46be
Michael Gilliland <mjg.py3@gmail.com>
2017-03-04 13:30:20 -0500

    Write an event to event store!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 'use strict';
 
 const Hapi = require('hapi');
+const uuid = require('node-uuid');
+const request = require('request-promise');
+const R = require('ramda');
+
+const esUrl = 'http://localhost:2113/';
+const streamUrl = streamName => `${esUrl}streams/${streamName}`;
+
+const produce = (event, stream) =>
+  request({
+    url: streamUrl(stream),
+    method: 'POST',
+    json: true,
+    body: [event],
+    headers: {
+      'Content-Type': 'application/vnd.eventstore.events+json'
+    }
+  });
 
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: 'localhost', routes: { cors: true } });
@@ -9,8 +26,24 @@ server.route({
   method: 'POST',
   path: '/recipe',
   handler: (request, reply) => {
-    console.log('Incoming recipe!');
-    reply({ message: "Nice" });
+    const event = {
+      eventId: uuid.v4(),
+      eventType: 'add-recipe',
+      data: request.payload
+    };
+    console.log(event);
+    produce(event, `Recipe-${event.data.recipe.name}`)
+      .then(
+        _ => {
+          console.log('Added');
+          reply({ message: 'Added' });
+        },
+        err => {
+          console.log('Error:', err);
+          reply({ message: 'An error occured' }).code(500);
+        }
+      );
+    ;
   }
 });
 
