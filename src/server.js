5450aa31a73259a0d6e0080f3f6b592b62647776
Michael Gilliland <mjg.py3@gmail.com>
2017-03-11 17:27:39 -0500

    Resolve #7. Bootstrap event store projections when starting server.

--- src/server.js
+++ src/server.js
@@ -4,10 +4,74 @@ const Hapi = require('hapi');
 const uuid = require('node-uuid');
 const request = require('request-promise');
 const R = require('ramda');
+const fs = require('fs');
 
 const esUrl = 'http://localhost:2113/';
 const streamUrl = streamName => `${esUrl}streams/${streamName}`;
 const projectionStateUrl = projectionName => `${esUrl}projection/${projectionName}/state`;
+
+const allProjections = () =>
+  request({
+    url: `${esUrl}projections/any`,
+    method: 'GET',
+    json: true,
+    headers: {
+      Accept: 'application/json'
+    }
+  });
+
+const createProjection = name =>
+  allProjections()
+    .then(
+      projections => {
+        if (!projections.projections.find(proj => proj.name === name)) {
+          request({
+            url: `${esUrl}projections/continuous?name=${name}&type=js&enabled=true&emit=false&trackemittedstreams=false`,
+            method: 'POST',
+            body: fs
+              .readFileSync(`projections/${name}.js`)
+              .toString('utf8'),
+            headers: {
+              Authorization: 'Basic YWRtaW46Y2hhbmdlaXQ=',
+              'Content-Type': 'text/plain'
+            }
+          })
+          .then(
+            _ => console.log(`Created "${name}" projection`),
+            err => console.log(`Failed to create "${name}", error:`, err)
+          )
+        } else {
+          console.log(`Projection "${name}" already exists - skipping`);
+        }
+      }
+    )
+    .then(
+      _ => 42,
+      err => console.log('Error: ', err)
+    );
+
+const enableByCategoryProjection = () =>
+  request({
+    url: `${esUrl}projection/$by_category/command/enable`,
+    method: 'POST',
+    json: true,
+    body: {},
+    headers: {
+      Authorization: 'Basic YWRtaW46Y2hhbmdlaXQ='
+    }
+  })
+  .then(
+    _ => console.log('Enabled "$by_category" projection'),
+    err => console.log(`Failed to enable "$by_category", error: ${err}`)
+  );
+
+const bootstrap = () =>
+  Promise.all([
+    enableByCategoryProjection()
+  , createProjection('all-recipe-names')
+  ]);
+
+bootstrap();
 
 const produce = (event, stream) =>
   request({
@@ -59,7 +123,7 @@ server.route({
     })
       .then(
         response => {
-          reply(response);
+          reply(response || []);
         },
         err => {
           console.log('Error:', err);
